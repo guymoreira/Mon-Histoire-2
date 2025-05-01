cf51ebc982341a4dfdcb239458463203aaeecc7c
guymoreira <moreiraguy@gmail.com>
2025-05-01 15:00:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+function generateStory() {
+  const story = "Il était une fois un petit aventurier courageux qui partit à la recherche d’un trésor magique au cœur de la forêt enchantée...";
+  document.getElementById('story').innerText = story;
+}
