0dc6ecfa547e6915385ad829d0de00374212dc8b
guymoreira <moreiraguy@gmail.com>
2025-05-01 15:02:42 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-function generateStory() {
-  const story = "Il était une fois un petit aventurier courageux qui partit à la recherche d’un trésor magique au cœur de la forêt enchantée...";
-  document.getElementById('story').innerText = story;
-}
