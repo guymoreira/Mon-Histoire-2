fcb024efc44a005c7b3fa43d1b4129f5300973ff
guymoreira <moreiraguy@gmail.com>
2025-05-03 01:06:57 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const btnConnexion = document.getElementById("btn-connexion");
-  if (btnConnexion) {
-    btnConnexion.addEventListener("click", function () {
-      window.location.href = "connexion.html";
-    });
-  }
-});
