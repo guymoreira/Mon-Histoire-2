28593a986f0f4f120238ae848761e8b053596e20
guymoreira <moreiraguy@gmail.com>
2025-05-03 19:03:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -178,32 +178,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-document.addEventListener("DOMContentLoaded", function () {
-  const isLoggedIn = localStorage.getItem("loggedIn") === "true";
-  const loginBtn = document.getElementById("login-btn");
-  const userIcon = document.getElementById("user-icon");
-
-  if (isLoggedIn) {
-    if (loginBtn) loginBtn.style.display = "none";
-    if (userIcon) userIcon.style.display = "flex";
-    userIcon.addEventListener("click", function () {
-      const modal = document.getElementById("logout-modal");
-      if (modal) modal.style.display = "block";
-    });
-  } else {
-    if (userIcon) userIcon.style.display = "none";
-  }
-});
-
-function logout() {
-  localStorage.removeItem("loggedIn");
-  window.location.href = "index.html";
-}
-document.addEventListener("DOMContentLoaded", () => {
-  const cancelButton = document.getElementById("cancel-button");
-  if (cancelButton) {
-    cancelButton.addEventListener("click", () => {
-      window.location.href = "index.html";
-    });
-  }
-});
