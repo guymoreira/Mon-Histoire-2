ad1b0c14c2bfac8900d4538ccb371feab2a4846c
guymoreira <moreiraguy@gmail.com>
2025-06-18 19:06:19 +0200

    refactor: re-export core cookies module

--- js/modules/features/cookies.js
+++ js/modules/features/cookies.js
@@ -1,46 +1,9 @@
 // js/modules/features/cookies.js
-// Wrapper module delegating to js/features/cookies.js
+// Re-export core cookies module for backward compatibility
 
 window.MonHistoire = window.MonHistoire || {};
 MonHistoire.modules = MonHistoire.modules || {};
+MonHistoire.modules.core = MonHistoire.modules.core || {};
 MonHistoire.modules.features = MonHistoire.modules.features || {};
 
-(function() {
-  const getFeature = () => MonHistoire.features && MonHistoire.features.cookies;
-  const apiMethods = [
-    'init',
-    'bindEvents',
-    'afficherBanniereCookies',
-    'masquerBanniereCookies',
-    'accepterTousCookies',
-    'ouvrirParametresCookies',
-    'mettreAJourInterrupteurs',
-    'sauvegarderPreferences',
-    'appliquerPreferences'
-  ];
-
-  const moduleAPI = {};
-  apiMethods.forEach(fn => {
-    moduleAPI[fn] = function(...args) {
-      const feature = getFeature();
-      if (feature && typeof feature[fn] === 'function') {
-        return feature[fn](...args);
-      }
-    };
-  });
-
-  Object.defineProperty(moduleAPI, 'preferences', {
-    get() {
-      const feature = getFeature();
-      return feature ? feature.preferences : undefined;
-    },
-    set(value) {
-      const feature = getFeature();
-      if (feature) {
-        feature.preferences = value;
-      }
-    }
-  });
-
-  MonHistoire.modules.features.cookies = moduleAPI;
-})();
+MonHistoire.modules.features.cookies = MonHistoire.modules.core.cookies;
