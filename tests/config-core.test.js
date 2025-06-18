bb9795e944154ff3994a363428014a2bb803cf5f
guymoreira <moreiraguy@gmail.com>
2025-06-19 00:14:58 +0200

    Add test for initFirebase persistence and update docs

---
+++ tests/config-core.test.js
@@ -0,0 +1,28 @@
+const { JSDOM } = require('jsdom');
+
+describe('core config module', () => {
+  let initFirebaseMock;
+
+  beforeEach(() => {
+    jest.resetModules();
+    const dom = new JSDOM('', { url: 'http://localhost' });
+    global.window = dom.window;
+    global.document = dom.window.document;
+    global.MonHistoire = { config: { initFirebase: jest.fn() }, modules: { core: {} } };
+    global.console = { log: jest.fn(), warn: jest.fn(), error: jest.fn() };
+    initFirebaseMock = global.MonHistoire.config.initFirebase;
+  });
+
+  afterEach(() => {
+    delete global.window;
+    delete global.document;
+    delete global.MonHistoire;
+    delete global.console;
+  });
+
+  test('initFirebase remains defined after loading core config', () => {
+    require('../js/modules/core/config.js');
+    expect(typeof window.MonHistoire.config.initFirebase).toBe('function');
+    expect(window.MonHistoire.config.initFirebase).toBe(initFirebaseMock);
+  });
+});
