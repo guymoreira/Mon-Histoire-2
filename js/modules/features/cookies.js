56ff6068afb29ba23f6aca1e8db5bcfd2607016b
guymoreira <moreiraguy@gmail.com>
2025-06-20 13:15:26 +0200

    Remove modules and adapters directories

--- js/modules/features/cookies.js
+++
@@ -1,9 +0,0 @@
-// js/modules/features/cookies.js
-// Re-export core cookies module for backward compatibility
-
-window.MonHistoire = window.MonHistoire || {};
-MonHistoire.modules = MonHistoire.modules || {};
-MonHistoire.modules.core = MonHistoire.modules.core || {};
-MonHistoire.modules.features = MonHistoire.modules.features || {};
-
-MonHistoire.modules.features.cookies = MonHistoire.modules.core.cookies;
